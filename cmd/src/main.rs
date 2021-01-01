fc958fe02c44a70996ad83ff2157b7bc7d6784e6
Nikita <nikitadiscore@gmail.com>
2021-01-01 16:04:07 +0300

    cmd: add auto create required files

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs::create_dir_all;
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
+
 use rufy_core::{
     Rufy,
     Result,
@@ -6,6 +11,15 @@ use rufy_core::{
 use rufy_engine::Engine;
 
 fn main() -> Result<(), Error> {
+    if !Path::new("project").exists() {
+        create_dir_all("project")?;
+        let mut file = File::create(Path::new("project/main.lua"))?;
+        file.write_all(b"
+function onInit()
+    -- onInit is main function which ruth calls at startup
+end")?;
+    }
+
     let engine = Engine::new();
     engine.init();
     let rufy = Rufy::new(String::from("project"))?;
