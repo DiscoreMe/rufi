0a71f439f2070de9c7bea6946672655090fa7f0b
Nikita <nikitadiscore@gmail.com>
2021-01-04 23:26:47 +0300

    cli: create new scene

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -9,10 +9,12 @@ use rufy_core::Rufy;
 fn main() {
     if !Path::new("project").exists() {
         create_dir_all("project").unwrap();
-        let mut file = File::create(Path::new("project/main.lua")).unwrap();
+        let mut file = File::create(Path::new( "project").join("main.lua")).unwrap();
         file.write_all(b"
 function onInit()
     -- onInit is main function which ruth calls at startup
+    -- run_scene(\"name\")
+    -- Uncommoment the line above. It is start the first scene when the app starts
 end").unwrap();
     }
 
@@ -24,6 +26,7 @@ end").unwrap();
     let usage = args[1].as_str();
     match usage {
         "run" => run(),
+        "create" => create(args),
         _ => help(),
     }
 }
@@ -31,9 +34,25 @@ end").unwrap();
 fn help() {
     println!("rufy --help\n");
     println!("USAGE: ");
-    println!("\t run \trun project in debug mode")
+    println!("\t run \t\trun project in debug mode");
+    println!("\t create [name]\tcreate new scene");
 }
 
 fn run() {
     Rufy::init(String::from("project"));
+}
+
+fn create(args: Vec<String>) {
+    if args.len() < 3 {
+        help();
+        return
+    }
+    let name = args[2].as_str();
+
+    create_dir_all(Path::new("project").join(name)).unwrap();
+    let mut file = File::create(Path::new( "project").join(name).join("main.lua")).unwrap();
+    file.write_all(b"
+function onInit()
+    -- onInit is main function which ruth calls scripts at scene
+end").unwrap();
 }
